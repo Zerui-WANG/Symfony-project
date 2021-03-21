2860a02541178b87dcc466ebba7961b0269ff3ad
Zerui-WANG <71635569+Zerui-WANG@users.noreply.github.com>
2021-03-21 17:14:46 +0100

    prepare to deploy

--- public/index.php
+++ public/index.php
@@ -7,7 +7,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+(new Dotenv())->bootEnv(dirname(__DIR__).'/htdocs/.env');
+
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
